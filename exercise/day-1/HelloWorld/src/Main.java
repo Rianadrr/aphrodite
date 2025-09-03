05f535e0892a9b343d5262475fda0613d32c1928
Rian Adriansyah <72530@fif.co.id>
2025-09-03 16:14:03 +0700

    adding HelloWorld project

--- exercise/day-1/HelloWorld/src/Main.java
+++ exercise/day-1/HelloWorld/src/Main.java
@@ -6,36 +6,36 @@ public class Main {
 //        System.out.println("Hello World!");
 
         // Tipe Data long
-//        long iniLong = 10;
-//        System.out.println("This is long : " + iniLong);
+        long iniLong = 10;
+        System.out.println("This is long : " + iniLong);
 
         // Tipe Data int
-//        int iniInt = 20;
-//        System.out.println("This is int : " + iniInt);
+        int iniInt = 20;
+        System.out.println("This is int : " + iniInt);
 
         // Tipe Data float
-//        float iniFloat = 10.11f;
-//        System.out.println("This is float : " + iniFloat);
+        float iniFloat = 10.11f;
+        System.out.println("This is float : " + iniFloat);
 
         // Tipe Data double
-//        double iniDouble = 10.10;
-//        System.out.println("This is double : " + iniDouble);
+        double iniDouble = 10.10;
+        System.out.println("This is double : " + iniDouble);
 
         // Tipe Data char
-//        char iniChar = 'A';
-//        System.out.println("This is char : " + iniChar);
+        char iniChar = 'A';
+        System.out.println("This is char : " + iniChar);
 
         // Tipe Data boolean
-//        boolean iniBoolean = true;
-//        System.out.println("This is boolean : " + iniBoolean);
+        boolean iniBoolean = true;
+        System.out.println("This is boolean : " + iniBoolean);
 
         // Tipe Data string
-//        String iniString = "Halo Halo Bandung";
-//        System.out.println("This is String : " + iniString);
+        String iniString = "Halo Halo Bandung";
+        System.out.println("This is String : " + iniString);
 
         // Tipe Data Array
-//        int[] iniArray = {10,20,30};
-//        System.out.println("This is array : " + iniArray[1]);
+        int[] iniArray = {10,20,30};
+        System.out.println("This is array : " + iniArray[1]);
 
 
 
