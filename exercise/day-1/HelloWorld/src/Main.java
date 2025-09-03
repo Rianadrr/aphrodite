ded26a2092e330b0147a317329838c61d7ec837b
Rian Adriansyah <72530@fif.co.id>
2025-09-03 15:47:31 +0700

    Menambahkan project HelloWorld

---
+++ exercise/day-1/HelloWorld/src/Main.java
@@ -0,0 +1,40 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello World!");
+
+        // Tipe Data long
+//        long iniLong = 10;
+//        System.out.println("This is long : " + iniLong);
+
+        // Tipe Data int
+//        int iniInt = 20;
+//        System.out.println("This is int : " + iniInt);
+
+        // Tipe Data float
+//        float iniFloat = 10.11f;
+//        System.out.println("This is float : " + iniFloat);
+
+        // Tipe Data double
+//        double iniDouble = 10.10;
+//        System.out.println("This is double : " + iniDouble);
+
+        // Tipe Data char
+//        char iniChar = 'A';
+//        System.out.println("This is char : " + iniChar);
+
+        // Tipe Data boolean
+//        boolean iniBoolean = true;
+//        System.out.println("This is boolean : " + iniBoolean);
+
+        // Tipe Data string
+//        String iniString = "Halo Halo Bandung";
+//        System.out.println("This is String : " + iniString);
+
+        // Tipe Data Array
+//        int[] iniArray = {10,20,30};
+//        System.out.println("This is array : " + iniArray[1]);
+    }
+}
